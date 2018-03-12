a8627823627b18d99f9a69247c210adb944a1011
Eric Smolen <esmolen2@gmail.com>
2018-03-12 17:45:24 -0400

    feat: Shuffle and arrange cards on page load and restart

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-/*
- * Create a list that holds all of your cards
- */
+//Select deck and create array of cards
 
+const deck = document.querySelector('.deck');
+const cards = Array.prototype.slice.call(document.getElementsByClassName('card'));
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// Shuffle & place the cards on the page
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// 1. Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,6 +20,30 @@ function shuffle(array) {
     return array;
 }
 
+// 2. Place the cards to arrange the entire deck on the page
+function placeCard(card) {
+    deck.appendChild(card);
+}
+
+function arrangeDeck(array) {
+    array.forEach(placeCard);
+}
+
+// 3a. Shuffling and arranging the deck on page load
+
+window.addEventListener('load', function () {
+    shuffle(cards);
+    arrangeDeck(cards);
+});
+
+// 3b. Shuffling and arranging the deck on clicking restart
+
+const restart = document.querySelector('.restart');
+
+restart.addEventListener('click', function () {
+    shuffle(cards);
+    arrangeDeck(cards);
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
