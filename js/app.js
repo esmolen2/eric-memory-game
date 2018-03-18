b59c6e20464648a5c33034c841baad03581b9f7f
Eric Smolen <esmolen2@gmail.com>
2018-03-18 17:35:35 -0400

    feat: Add timer and moves counter

--- js/app.js
+++ js/app.js
@@ -85,13 +85,67 @@ function hideAllSymbols(array) {
     array.forEach(hideMatch);
 }
 
+// Control display and count of timer
+
+let seconds = 0;
+const timerDisplay = document.querySelector('.timer');
+const secondsDisplay = document.querySelector('.seconds');
+let countInterval = null;
+
+function countUp() {
+    secondsDisplay.innerHTML = ++seconds;
+}
+
+function startCount() {
+    countInterval = setInterval(countUp, 1000);
+    addClass(timerDisplay, 'visible')
+    deck.removeEventListener('click', startCount);
+}
+
+function stopCount() {
+    clearInterval(countInterval);
+}
+
+function resetTimer() {
+    stopCount();
+    seconds = 0;
+    secondsDisplay.innerHTML = seconds;
+    removeClass(timerDisplay, 'visible');
+    deck.addEventListener('click', startCount);
+}
+
+// Control move counter
+
+let clicks = 0;
+let moves = 0;
+const movesDisplay = document.querySelector('.moves');
+
+function moveCounter() {
+    clicks += 1;
+    if (clicks !== 0 && clicks % 2 === 0) {
+        moves += 1;
+        movesDisplay.innerHTML = moves;
+    };
+}
+
+function clearMoves() {
+    clicks = 0;
+    moves = 0;
+    movesDisplay.innerHTML = moves;
+}
+
 // Control display of the winning screen
 
 const winningScreen = document.getElementById('winning-screen');
+const winningSeconds = winningScreen.querySelector('.seconds');
+const winningMoves = winningScreen.querySelector('.moves');
 const playAgain = document.querySelector('.play-again');
 
 function showWinner() {
     if (cards.length === matchedCardsList.length) {
+      stopCount();
+      winningSeconds.innerHTML = seconds;
+      winningMoves.innerHTML = moves;
       addClass(winningScreen, 'flex');
     };
 };
@@ -104,6 +158,7 @@ function hideWinner() {
 
 function openCards(event) {
   if (event.target.classList.contains('card') === true) {
+    moveCounter();
     const firstCard = openCardsList[0];
     if (openCardsList.length) {
         if (firstCard.innerHTML === event.target.innerHTML) {
@@ -141,23 +196,32 @@ restart.addEventListener('click', function () {
     hideAllSymbols(cards);
     clearList(openCardsList);
     clearList(matchedCardsList);
+    resetTimer();
+    clearMoves();
     shuffle(cards);
     arrangeDeck(cards);
 });
 
 // Clicking cards
+
 deck.addEventListener('click', function () {
     showSymbol(event);
     openCards(event);
 });
 
+// Start & display timer
+
+deck.addEventListener('click', startCount);
+
 // Winning screen
 
 playAgain.addEventListener('click', function () {
-  hideWinner()
+  hideWinner();
   hideAllSymbols(cards);
   clearList(openCardsList);
   clearList(matchedCardsList);
+  resetTimer();
+  clearMoves();
   shuffle(cards);
   arrangeDeck(cards);
 });
